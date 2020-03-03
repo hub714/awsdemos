7982fad9a1d2de5f77f3d7b23567420382656a6e
hub714 <hubertcheung@gmail.com>
2020-03-02 17:45:19 -0800

    using jsonlines to write manifest based in images folder

--- sm-groundtruth/setup.py
+++ sm-groundtruth/setup.py
@@ -1,7 +1,17 @@
 import boto3
+import jsonlines
 from os import listdir
 from os.path import isfile, join
 
 bucketName = 'huberttest'
+bucketPath = 'sm-groundtruth'
 
-print(listdir('images'))
+outputJSON = []
+
+for f in listdir('images'):
+    outputJSON.append({'source-ref': f})
+
+#print(outputJSON)
+
+with jsonlines.open('manifest.json', 'w') as outfile:
+    outfile.write_all(outputJSON)
