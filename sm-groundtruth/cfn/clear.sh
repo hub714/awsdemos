d91239657c9f6098d79cedb79a3821178ece145d
hub714 <hubertcheung@gmail.com>
2020-03-30 17:38:03 -0700

    CFN flow for creating Cognito and a workforce done

--- sm-groundtruth/cfn/clear.sh
+++ sm-groundtruth/cfn/clear.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# aws cloudformation delete-stack --stack-name "groundtruth-test" --region us-east-1
+
 aws cloudformation delete-stack --stack-name "groundtruth-test" --region us-west-2
--- sm-groundtruth/cfn/create.sh
+++ sm-groundtruth/cfn/create.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# aws cloudformation create-stack --stack-name "groundtruth-test" --region us-east-1 --template-body file://core.yml
+
 aws cloudformation create-stack --stack-name "groundtruth-test" --region us-west-2 --template-body file://core.yml
